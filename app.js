d1f4622917aee69b4e874b56a6e4abdbd122e487
Rodrigo Brandao <rodrigo.brandao@gmail.com>
2017-07-27 20:51:12 -0300

    .

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var path = require('path');
+
 //var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var hbs = require('express-handlebars');
-
 
 var routes = require('./routes/index');
 
